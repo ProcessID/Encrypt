92228f9726322f58c7e1f783cf74dfde1e09f6e6
processid <34098270+processid@users.noreply.github.com>
2020-08-18 19:02:04 +0200

    Add files via upload

---
+++ EncryptFileOpenSSL.php
@@ -0,0 +1,117 @@
+<?php
+// Chiffrement/Déchiffrement de fichier avec OpenSSL
+// -------------------
+// -- Instanciation --
+// -------------------
+// $obj = new EncryptFileOpenSSL($key_aes256, $key_hash512, $method);
+// $key_aes256 = <key aes256>
+// $key_hash512 = <key hash512>
+// $method = <'aes-128-cbc' | 'aes-256-cbc' | ...>
+
+namespace processid\encrypt;
+
+class EncryptFileOpenSSL {
+    private $_password;
+    private $_method;
+
+    protected $_file_in;
+    protected $_file_out;
+    
+    static $FILE_ENCRYPTION_BLOCKS = 10000;
+
+    public function __construct($password, $method) {
+        $this->SetPassword($password);
+        $this->SetMethod($method);
+    }
+
+    public function SetPassword($password) {
+        $this->_password = $password;
+    }
+
+    public function SetMethod($method) {
+        $this->_method = $method;
+    }
+
+    public function SetFile_in($file_in) {
+        $this->_file_in = $file_in;
+    }
+
+    public function SetFile_in($file_out) {
+        $this->_file_out = $file_out;
+    }
+
+    private function password() {
+        return $this->_password;
+    }
+
+    private function method() {
+        return $this->_method;
+    }
+
+    public function file_in() {
+        return $this->_file_in;
+    }
+
+    public function file_out() {
+        return $this->_file_out;
+    }
+
+    function encrypt_file($file_in, $file_out) {
+        $iv_length = openssl_cipher_iv_length($this->method());
+        $key = substr(sha1($this->password(), true), 0, 16);
+        $iv = openssl_random_pseudo_bytes($iv_length);
+
+        $error = false;
+        if ($fpOut = fopen($file_out, 'w')) {
+            // Enregistrement du vecteur d'initialisation au début du fichier
+            fwrite($fpOut, $iv);
+            if ($fpIn = fopen($file_in, 'rb')) {
+                while (!feof($fpIn)) {
+                    $plaintext = fread($fpIn, 16 * self::$FILE_ENCRYPTION_BLOCKS);
+                    $ciphertext = openssl_encrypt($plaintext, $this->method(), $key, OPENSSL_RAW_DATA, $iv);
+                    // On utilise les $iv_length octets de ciphertext comme prochain vecteur d'initialisation
+                    $iv = substr($ciphertext, 0, $iv_length);
+                    fwrite($fpOut, $ciphertext);
+                }
+                fclose($fpIn);
+            } else {
+                $error = true;
+            }
+            fclose($fpOut);
+        } else {
+            $error = true;
+        }
+
+        return $error ? false : true;
+    }
+
+    function decrypt_file($file_in, $file_out) {
+        $iv_length = openssl_cipher_iv_length($this->method());
+        $key = substr(sha1($this->password(), true), 0, 16);
+
+        $error = false;
+        if ($fpOut = fopen($file_out, 'w')) {
+            if ($fpIn = fopen($file_in, 'rb')) {
+                // Lecture du vecteur d'initialisation au début du fichier
+                $iv = fread($fpIn, $iv_length);
+                while (!feof($fpIn)) {
+                    // Il faut lire un bloc de plus pour déchiffrer que pour chiffrer
+                    $ciphertext = fread($fpIn, 16 * (self::$FILE_ENCRYPTION_BLOCKS + 1)); 
+                    $plaintext = openssl_decrypt($ciphertext, $this->method(), $key, OPENSSL_RAW_DATA, $iv);
+                    // On utilise les $iv_length octets de ciphertext comme prochain vecteur d'initialisation
+                    $iv = substr($ciphertext, 0, $iv_length);
+                    fwrite($fpOut, $plaintext);
+                }
+                fclose($fpIn);
+            } else {
+                $error = true;
+            }
+            fclose($fpOut);
+        } else {
+            $error = true;
+        }
+
+        return $error ? false : true;
+    }
+}
+?>
--- EncryptOpenSSL.php
+++ EncryptOpenSSL.php
@@ -8,10 +8,10 @@
    // -------------------
    // -- Instanciation --
    // -------------------
-   // $obj = new EncryptOpenSSL($key_aes256, $key_hash512, $methode);
+   // $obj = new EncryptOpenSSL($key_aes256, $key_hash512, $method);
    // $key_aes256 = <key aes256>
    // $key_hash512 = <key hash512>
-   // $methode = <'aes-256-cbc' | ...>
+   // $method = <'aes-128-cbc' | 'aes-256-cbc' | ...>
    
    
    namespace processid\encrypt;
@@ -19,12 +19,12 @@
    class EncryptOpenSSL {
       private $_key_aes256;
       private $_key_hash512;
-      private $_methode;
+      private $_method;
       
-      function __construct($key_aes256, $key_hash512, $methode) {
+      function __construct($key_aes256, $key_hash512, $method) {
          $this->SetKey_aes256($key_aes256);
          $this->SetKey_hash512($key_hash512);
-         $this->SetMethode($methode);
+         $this->SetMethod($method);
       }
       
       function SetKey_aes256($key_aes256) {
@@ -35,8 +35,8 @@
          $this->_key_hash512 = $key_hash512;
       }
       
-      function SetMethode($methode) {
-         $this->_methode = $methode;
+      function SetMethod($method) {
+         $this->_method = $method;
       }
       
       private function key_aes256() {
@@ -47,19 +47,19 @@
          return $this->_key_hash512;
       }
       
-      private function methode() {
-         return $this->_methode;
+      private function method() {
+         return $this->_method;
       }
       
       function encrypt_string($data) {
          $key_aes256 = base64_decode($this->key_aes256());
          $key_hash512 = base64_decode($this->key_hash512());
          
-         $iv_length = openssl_cipher_iv_length($this->methode());
+         $iv_length = openssl_cipher_iv_length($this->method());
          $iv = openssl_random_pseudo_bytes($iv_length);
 
          // À la place de '1', il faudrait utiliser la constante OPENSSL_RAW_DATA qui vaut 1, mais elle n'existe pas en PHP 5.3 où il fallait passer 'true', donc la valeur 1 est parfaite pour toutes les versions
-         $first_encrypted = openssl_encrypt($data, $this->methode(), $key_aes256, 1, $iv);
+         $first_encrypted = openssl_encrypt($data, $this->method(), $key_aes256, 1, $iv);
          $second_encrypted = hash_hmac('sha512', $first_encrypted, $key_hash512, TRUE);
 
          $output = base64_encode($iv.$second_encrypted.$first_encrypted);
@@ -72,14 +72,14 @@
          $key_hash512 = base64_decode($this->key_hash512());
          $mix = base64_decode($data);
          
-         $iv_length = openssl_cipher_iv_length($this->methode());
+         $iv_length = openssl_cipher_iv_length($this->method());
 
          $iv = substr($mix,0,$iv_length);
          $second_encrypted = substr($mix, $iv_length, 64);
          $first_encrypted = substr($mix, $iv_length+64);
 
          // À la place de '1', il faudrait utiliser la constante OPENSSL_RAW_DATA qui vaut 1, mais elle n'existe pas en PHP 5.3 où il fallait passer 'true', donc la valeur 1 est parfaite pour toutes les versions
-         $data = openssl_decrypt($first_encrypted, $this->methode(), $key_aes256, 1, $iv);
+         $data = openssl_decrypt($first_encrypted, $this->method(), $key_aes256, 1, $iv);
          $second_encrypted_new = hash_hmac('sha512', $first_encrypted, $key_hash512, TRUE);
 
          // La fonction hash_equals() n'existe qu'à partir de PHP 5.6
